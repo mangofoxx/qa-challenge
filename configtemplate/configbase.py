6ad1ded8059fea1a6be2448d1d6d63567e41e2cb
tijanac <tijana.cukic@sbgenomics.com>
2021-01-30 17:52:51 +0100

    Fill in `TestConfig`

--- configtemplate/configbase.py
+++ configtemplate/configbase.py
@@ -1,3 +1,7 @@
 class TestConfig:
-    IMPLICIT_WAIT = 10
     BASE_URL = 'https://doodle.com/en'
+    LOGIN_PAGE_URL = 'https://doodle.com/login'
+    USER_DASHBOARD = 'https://doodle.com/dashboard'
+
+    USER_EMAIL = 'tijanaqa101@gmail.com'
+    USER_PASSWD = 'Qautotest123'
