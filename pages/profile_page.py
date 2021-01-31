3f49d13351d52b58bdebb8d5910909e6f44aa0b1
tijanac <tijana.cukic@sbgenomics.com>
2021-01-31 14:55:30 +0100

    Update `ProfilePage`

--- pages/profile_page.py
+++ pages/profile_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
-from configtemplate.configbase import TestConfig
 from .base import BasePage
+from configtemplate.configbase import TestConfig
 
 
 class ProfilePage(BasePage):
@@ -9,6 +9,12 @@ class ProfilePage(BasePage):
     PROFILE_PAGE_TITLE = 'Doodle – Dashboard'
 
     USER_MENU = (By.CSS_SELECTOR, '.UserMenu')
+    CREATE_POLL = (By.CSS_SELECTOR, '.CreatePollMenu')
+
+    CREATE_POLL_MENU = (By.CSS_SELECTOR, 'ul.Menu-list[role="menu"]')
+    MENU_ITEM_SURVEY = (
+        By.CSS_SELECTOR, '[data-testid="navigation-menu-create-text-poll"]'
+    )
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -22,8 +28,23 @@ class ProfilePage(BasePage):
             message='Could not match page title'
         )
 
+    def open_menu__create(self):
+        self.wait_for_create_menu().click()
+
+    def menu_option_click__survey(self):
+        return self.execute_click(
+            self.MENU_ITEM_SURVEY,
+            message='Could not click on menu item Survey'
+        )
+
     def wait_for_user_menu(self):
-        self.wait_for_element_to_be_clickable(
+        return self.wait_for_element_to_be_clickable(
             self.USER_MENU,
             message='User menu is not clickable'
+        )
+
+    def wait_for_create_menu(self):
+        return self.wait_for_element_to_be_clickable(
+            self.CREATE_POLL,
+            message='+Create menu is not clickable'
         )
