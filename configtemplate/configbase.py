9800f2050664bacb50b5b9b4ff23580b60194ce6
tijanac <tijana.cukic@sbgenomics.com>
2021-02-01 20:12:43 +0100

    Describe `TestConfig` class

--- configtemplate/configbase.py
+++ configtemplate/configbase.py
@@ -1,4 +1,5 @@
 class TestConfig:
+    """Base static configuration"""
     BASE_URL = 'https://doodle.com/en'
     LOGIN_PAGE_URL = 'https://doodle.com/login'
     USER_DASHBOARD = 'https://doodle.com/dashboard'
