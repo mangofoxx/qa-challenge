ff0c402b878a6d736db9c872320f26c943cc3313
tijanac <tijana.cukic@sbgenomics.com>
2021-02-01 20:17:45 +0100

    Remove firefox support

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,13 +7,10 @@ from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture(params=['chrome'], scope='class')
 def init_driver(request):
-    web_driver = None
-
     if request.param == 'chrome':
         web_driver = webdriver.Chrome(ChromeDriverManager().install())
-    if request.param == 'firefox':
-        web_driver = webdriver.Firefox(GeckoDriverManager().install())
+    else:
+        raise NotImplementedError('Currently supporting Chrome')
     request.cls.driver = web_driver
-    web_driver.maximize_window()
     yield
     web_driver.close()
